c5e954bce88c3927693203676bcec0b38031ce2b
Petr Kratochvíl <56606404+cratas@users.noreply.github.com>
2024-02-18 21:41:36 +0100

    fix(embedding): using default embedding has better results (#4)

--- src/utils/get-pinecone-store.ts
+++ src/utils/get-pinecone-store.ts
@@ -1,7 +1,6 @@
 import { Pinecone } from '@pinecone-database/pinecone';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from '@langchain/pinecone';
-import { EMBEDDING_MODEL_NAME } from '@/config-global';
 
 if (!process.env.PINECONE_API_KEY || !process.env.PINECONE_INDEX) {
   throw new Error('Pinecone environment or api key vars missing');
@@ -9,7 +8,7 @@ if (!process.env.PINECONE_API_KEY || !process.env.PINECONE_INDEX) {
 
 export const getPineconeStore = async (namespace: string) => {
   const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY as string });
-  const embedder = new OpenAIEmbeddings({ modelName: EMBEDDING_MODEL_NAME });
+  const embedder = new OpenAIEmbeddings();
 
   const pcIndex = pc.Index(process.env.PINECONE_INDEX as string);
 
